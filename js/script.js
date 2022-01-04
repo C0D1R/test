a41d88ddaf41af022cb26a09fb03a023a98180fa
C0D1R <gp23456z@gmail.com>
2022-01-04 23:57:33 +0800

    test

--- js/script.js
+++ js/script.js
@@ -30,7 +30,7 @@ btn.addEventListener("click", function() {
 
     (function() {
         $('#list').html("");
-        const url = "https://script.google.com/macros/s/AKfycbwPSC9YMKIQCRk6CGQAP2WQ-6kCzDWdytStpGDex--Yi38KnIj-3QEyE3o5zfAwkNgy/exec";
+        const url = "https://script.google.com/macros/s/AKfycbxNoA9HDtvXRpkXzatjBrFo8TCThF5aBUV7WLetYRXv23C-xjKXAC5I2xjlmRtscom99A/exec";
         /*
         const lang = document.getElementsByName("language");
         const sem = document.getElementsByName("semester");
