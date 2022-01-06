9992446ac46f23ee1969a3f1349fa6a0c703a253
C0D1R <gp23456z@gmail.com>
2022-01-07 00:51:46 +0800

    test

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const btn = document.getElementById("btn");
 const list = document.getElementById("list");
 
 function getRadioBoxValue(RadioBoxValue) {
-    for(let i = 0, len = RadioBoxValue.length; i < len; i++) {
+    for(let i = RadioBoxValue.length-1; i >= 0; i--) {
         if(RadioBoxValue[i].checked) {
             return RadioBoxValue[i].value;
         }
@@ -10,26 +10,116 @@ function getRadioBoxValue(RadioBoxValue) {
 }
 function getCheckBoxValue(CheckBoxValue) {
     let CheckedValue = [];
-    for(let i = 0, j = 0, len = CheckBoxValue.length; i < len; i++) {
+    for(let i = CheckBoxValue.length-1, j = 0; i >= 0; i--) {
         if(CheckBoxValue[i].checked) {
             CheckedValue[j++] = CheckBoxValue[i].value;
         }
     }
-    console.log(CheckedValue);
     return CheckedValue;
 }
 
 window.onload = function() {
     if(sessionStorage.getItem("CourseList")) {
-        list.innerHTML = sessionStorage.getItem("CourseList");
-    }
+        list.innerHTML = sessionStorage.getItem("CourseList")
+    };
 }
 
 btn.addEventListener("click", function() {
     sessionStorage.clear();
     list.innerHTML = "";
 
-    console.log(getRadioBoxValue(document.getElementsByName("department")));
+    (function() {
+        let html = "", url = [];
+        (function() {
+            const semester = getRadioBoxValue(document.getElementsByName("semester"));
+            const schoolsystem = getRadioBoxValue(document.getElementsByName("schoolsystem"));
+            const department =  getRadioBoxValue(document.getElementsByName("department"));
+            const coursetype = getCheckBoxValue(document.getElementsByName("course_type"));
+            for(let i = coursetype.length-1; i >= 0; i--) {//length
+                url.push(coursetype[i] != "general_elective_subject"?
+                        "./data/" + semester + "_" + schoolsystem + "_" + department + "_" + coursetype + ".json":
+                        "./data/" + semester + "_" + getGeneralField(department) + ".json");
+            }
+        }());
+        (function() {
+            const request = new XMLHttpRequest();
+            for(let i = url.length-1; i >= 0; i--) {
+                request.open("GET", url[i]);
+                request.send(null);
+                request.onload = function() {
+                    if(request.status == 200) {
+                        const data = JSON.parse(request.responseText);
+                        for(let i = data.length-1, temp = null; i >= 0 && (temp = data[i]); i--) {
+                            html += `
+                                    <div>
+                                        <div style="width: 30%; padding-left: 5%;" class="float datalistli">${temp.list_name}</div>
+                                        <div style="width: 7%;" class="float datalistli">${temp.list_credit}</div>
+                                        <div style="width: 18%;" class="float datalistli textsetmid">${temp.list_lecturer}</div>
+                                        <div style="width: 13%;" class="float datalistli textsetmid">${temp.list_type}</div>
+                                        <div style="width: 17%;" class="float datalistli textsetmid">${temp.list_field_class}</div>
+                                        <div style="width: 10%;" class="float datalistli">${temp.list_time}</div>
+                                        <div style="clear: both;"></div>
+                                    </div>
+                                    `;
+                        }
+                    }
+                }
+            }
+        }());
+        list.innerHTML += html;
+        sessionStorage.setItem("CourseList", html);
+    }());
+
+});
+
+function addlist_course_th() {
+    list.innerHTML += ` <div>
+                            <div style="width: 30%; padding-left: 5%;" class="float datalistlith">課程名稱</div>
+                            <div style="width: 7%;" class="float datalistlith">學分</div>
+                            <div style="width: 18%;" class="float datalistlith">任課教師</div>
+                            <div style="width: 13%;" class="float datalistlith">課程類別</div>
+                            <div style="width: 17%;" class="float datalistlith">班級／領域</div>
+                            <div style="width: 10%;" class="float datalistlith">時間</div>
+                            <div style="clear: both;"></div>
+                        </div>`;
+}
+
+function getGeneralField() {
+    switch(deptValue) {
+        case "ee":
+        case "mech":
+        case "eecs":
+        case "oe":
+        case "csie":
+        case "chem":
+        case "bio":
+        case "vc":
+        case "ic":
+        case "mes":
+        case "cpd":
+        case "pmi":
+            return ["humanities_and_arts", "social_science", "comprehensive_practice"];
+        case "imi":
+        case "ib":
+        case "accinfo":
+        case "ba":
+        case "fin":
+        case "leisure":
+        case "mim":
+        case "mis":
+        case "hm":
+        case "english":
+        case "japan":
+        case "childcare":
+        case "ss":
+            return ["humanities_and_arts", "natural_science", "comprehensive_practice"];
+        default:
+            return ["humanities_and_arts", "social_science", "natural_science", "comprehensive_practice"];
+    }
+}
+
+
+/*
     (function() {
         $('#list').html("");
         const url = "https://script.google.com/macros/s/AKfycbwe4Jglb_iKglwI1mIrZ7YwPFEFWaBG9Sy8xwicB4z_6YQf2QaSk2-V0VM5iSxR5k5oHg/exec";
@@ -39,6 +129,8 @@ btn.addEventListener("click", function() {
         const dept = document.getElementsByName("department");
         const courty = document.getElementsByName("course_type");
         */
+
+        /*
         $.ajax({
             type: 'GET',
             url: url,
@@ -70,5 +162,4 @@ btn.addEventListener("click", function() {
                 sessionStorage.setItem("CourseList", html);
             }
         });
-    }());
-});
+    }());*/
